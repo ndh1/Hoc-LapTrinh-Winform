0340e514c48324515245ef0dac9b1fc3233a63d3
ndh1 <ndh1499@gmail.com>
2021-07-24 04:02:59 +0700

    test3

--- baitapc/main.c
+++
@@ -1,7 +0,0 @@
-int main(){
-	printf("%d \n",tongchan(10));
-	printf("%d \n",tongle(10));
-	printf("%d \n",giaithua(10));
-	printf("%d \n",somu(8,4));
-	return 0;
-}
--- baitapc/thuvien.h
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int giaithua(int a);
-int tongchan(int a);
-int tongle(int a);
-int somu(int a, int b);
